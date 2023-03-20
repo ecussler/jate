199c1137fe0d72c9844bb3022bf9a65f8e1419f7
E Cussler <cusslere@gmail.com>
2023-03-20 16:39:36 -0700

    Pushing to Heroku:

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -19,7 +19,7 @@ export const putDb = async (content) => {
   const jateDb = await openDB('jate', 1); 
   const tx = jateDb.transaction('jate', 'readwrite'); 
   const store = tx.objectStore('jate'); 
-  const request = store.add({ text: content }); 
+  const request = store.put({ id: 1, value: content }); 
   const result = await request; 
 
   console.log('Data saved to database!', result); 
@@ -27,17 +27,17 @@ export const putDb = async (content) => {
 
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => {
+export const getDb = async (e) => {
   console.log("GET all from database"); 
 
   const jateDb = await openDB('jate', 1); 
   const tx = jateDb.transaction('jate', 'readonly'); 
   const store = tx.objectStore('jate'); 
-  const request = store.getAll(); 
+  const request = store.get(1); 
   const result = await request; 
 
   console.log("result.value", result); 
-  return result; 
+  return result.value; 
 };
 
 initdb();
